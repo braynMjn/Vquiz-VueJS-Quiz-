0b8a74d89bf33d38a6966126715773d831c3ef4e
braynMjn <brainmjn11@gmail.com>
2020-11-20 00:56:08 +0545

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <b-nav>
+            <b-nav-item disable> <b> Fancy Quiz VueApp </b></b-nav-item>
+            <b-nav-item disable> Counter: {{ numCorrect }} / {{ numTotal }} </b-nav-item>
+        </b-nav>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'numCorrect',
+        'numTotal'
+    ]
+}
+</script>
+
+<style scoped>
+div{
+    margin-left: 35%;
+    font-size: 22px;
+}
+</style>
